26dd19d59bd9046108685424f2e4e75037d3ff81
MazzGuille <mazzcode@gmail.com>
2022-06-07 20:16:47 -0300

    Agregar archivos de proyecto.

---
+++ Modelo/Persona.cs
@@ -0,0 +1,30 @@
+﻿namespace clase13.Modelo
+{
+    public class Persona
+    {
+        public Persona()
+        {
+            WriteLine("Se ha instancuado una persona");
+        }
+
+        public string? NombreCompleto { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public int Edad { get; set; }
+        public bool EstaCasado { get; set; }
+
+        public string ObtenerSaludo()
+        {
+            return ($"Hola soy {NombreCompleto}");
+        }
+
+        public string ObtenerSaludo(string NombreDeLaPersona)
+        {
+            return ($"Hola {NombreDeLaPersona}, soy {NombreCompleto}");
+        }
+
+        public void Saludo()
+        {
+            WriteLine("Hola desde la clase");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿global using static System.Console;
+using clase13.Modelo;
+
+
+
+WriteLine("Hello");
+
+
+Persona sujeto1 = new()
+{
+    NombreCompleto = "Guillermo Mazzari",
+    NumeroDocumento = "95674765",
+    Edad = 36,
+    EstaCasado = true
+};
+
+sujeto1.Saludo();
+WriteLine(sujeto1.ObtenerSaludo());
+WriteLine(sujeto1.ObtenerSaludo("July"));
